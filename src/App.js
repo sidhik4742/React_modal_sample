39f3f86648f03261b2847671be67ec121ecfa8a1
ayu123singh <ayushikumari2496@gmail.com>
2021-07-28 23:41:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import Modal from "modal-vanilla";
 
 function App() {
+  const modelHandler = () => {
+    new Modal({ el: document.getElementById("myModal") }).show();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,15 +15,74 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={modelHandler}
         >
-          Learn React
-        </a>
+          Launch modal
+        </button>
       </header>
+
+      <div
+        className="modal fade"
+        id="myModal"
+        tabIndex={-1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                New message
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-dismiss="modal"
+                aria-label="Close"
+              />
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="recipient-name" className="col-form-label">
+                    Recipient:
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="recipient-name"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="message-text" className="col-form-label">
+                    Message:
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="message-text"
+                    defaultValue={""}
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Send message
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
